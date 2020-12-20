0cb99a6533dcc47b6ab52548c5296187d80b9d7b
Bart Piasek <bartpiasek@icloud.com>
2020-12-20 21:12:22 +0100

    burgerbuilder component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
+import burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>BigMacReact</div>
+        <burger />
         <div>Build Controls</div>
       </Aux>
     );
