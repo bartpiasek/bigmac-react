2ef1c05f47d3e3ecf92b9c1fcde54f39df69a8a7
Bart Piasek <bartpiasek@icloud.com>
2020-12-22 19:47:56 +0100

    connect state to build controls - add button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -5,7 +5,9 @@ const buildControl = (props) => (
   <div className={classes.buildControl}>
     <div className={classes.Label}>{props.label}</div>
     <button className={classes.Less}>Remove</button>
-    <button className={classes.More}>Add</button>
+    <button className={classes.More} onClick={props.added}>
+      Add
+    </button>
   </div>
 );
 
