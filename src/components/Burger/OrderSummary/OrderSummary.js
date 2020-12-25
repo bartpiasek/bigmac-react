d3b424f2b4e552893b3dac9099c6f775221025f6
Bart Piasek <bartpiasek@icloud.com>
2020-12-25 18:39:25 +0100

    button component with css classes condition

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,8 @@ const orderSummary = (props) => {
       <p>Burger with following ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout</p>
+      <button>Cancel</button>
+      <button>Continue</button>
     </Aux>
   );
 };
