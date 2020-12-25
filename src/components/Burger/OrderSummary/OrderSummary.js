502c651dfe51de701cf33a726cb3aa9bbc9d67c4
Bart Piasek <bartpiasek@icloud.com>
2020-12-25 19:53:16 +0100

    total price in modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
   // ingredient to list items
@@ -16,9 +17,16 @@ const orderSummary = (props) => {
       <h3>Your order</h3>
       <p>Burger with following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        Total price: <strong>{props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout</p>
-      <button>Cancel</button>
-      <button>Continue</button>
+      <Button btnType="Danger" clicked={props.purchaseCanceled}>
+        Cancel
+      </Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>
+        Continue
+      </Button>
     </Aux>
   );
 };
