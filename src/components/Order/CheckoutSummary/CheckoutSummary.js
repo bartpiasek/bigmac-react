9b937f4044ebac84a25b011a05aa7a7b08ab7abf
Bart Piasek <bartpiasek@icloud.com>
2021-01-12 22:13:48 +0100

    data form

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -6,7 +6,7 @@ import classes from "./CheckoutSummary.css";
 const checkoutSummary = (props) => {
   return (
     <div className={classes.CheckoutSummary}>
-      <h2>We hope it tastes well!</h2>
+      <h1>We hope it tastes well!</h1>
       <div style={{ width: "100%", margin: "auto" }}>
         <Burger ingredients={props.ingredients} />
       </div>
