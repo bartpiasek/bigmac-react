2788aa97c81d400528471e8550a83a75112641b1
Bart Piasek <bartpiasek@icloud.com>
2021-01-14 22:49:05 +0100

    check validity - form validation

--- src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
+++ src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
@@ -15,6 +15,10 @@ class ContactData extends Component {
           placeholder: "Your name",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       street: {
         elementType: "input",
@@ -23,6 +27,10 @@ class ContactData extends Component {
           placeholder: "Street",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       zipCode: {
         elementType: "input",
@@ -31,6 +39,11 @@ class ContactData extends Component {
           placeholder: "Postal code",
         },
         value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+        },
+        valid: false,
       },
       country: {
         elementType: "input",
@@ -39,6 +52,10 @@ class ContactData extends Component {
           placeholder: "Country",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       email: {
         elementType: "input",
@@ -47,6 +64,10 @@ class ContactData extends Component {
           placeholder: "Email",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
       },
       deliveryMethod: {
         elementType: "select",
@@ -93,13 +114,32 @@ class ContactData extends Component {
       });
   };
 
+  checkValidity(value, rules) {
+    let isValid = false;
+
+    if (rules.required) {
+      isValid = value.trim() !== "";
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = { ...this.state.orderForm };
     //inputIdentifier - email, name, street etc
     const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
 
     updatedFormElement.value = event.target.value;
+
+    updatedFormElement.valid = this.checkValidity(
+      updatedFormElement.value,
+      updatedFormElement.validation
+    );
+
     updatedOrderForm[inputIdentifier] = updatedFormElement;
+    console.log(updatedFormElement);
     this.setState({ orderForm: updatedOrderForm });
   };
 
