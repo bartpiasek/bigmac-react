e3c20db8f6dacc8031b32ea85d8b01d15a4f4f45
Bart Piasek <bartpiasek@icloud.com>
2021-01-15 20:14:08 +0100

    check validation feedback - all inputs error

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,11 +3,18 @@ import classes from "./Input.css";
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [classes.inputElement];
+
+  // if invalid - push invalid as a css class
+  if (props.invalid && props.shouldValidate) {
+    inputClasses.push(classes.Invalid);
+  }
+
   switch (props.elementType) {
     case "input":
       inputElement = (
         <input
-          className={classes.inputElement}
+          className={inputClasses.join(" ")}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -17,7 +24,7 @@ const input = (props) => {
     case "textarea":
       inputElement = (
         <textarea
-          className={classes.inputElement}
+          className={inputClasses}
           {...props.elementConfig}
           value={props.value}
           onChange={props.changed}
@@ -27,7 +34,7 @@ const input = (props) => {
     case "select":
       inputElement = (
         <select
-          className={classes.inputElement}
+          className={inputClasses}
           value={props.value}
           onChange={props.changed}
         >
--- src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
+++ src/containers/BurgerBuilder/Checkout/ContactData/ContactData.js
@@ -115,13 +115,13 @@ class ContactData extends Component {
   };
 
   checkValidity(value, rules) {
-    let isValid = false;
+    let isValid = true;
 
     if (rules.required) {
-      isValid = value.trim() !== "";
+      isValid = value.trim() !== "" && isValid;
     }
     if (rules.minLength) {
-      isValid = value.length >= rules.minLength;
+      isValid = value.length >= rules.minLength && isValid;
     }
     return isValid;
   }
@@ -162,6 +162,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            // dropdown don't have validation - dropdown is not treat as invalid
+            shouldValidate={formElement.config.validation}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
