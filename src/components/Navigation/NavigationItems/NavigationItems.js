9d4eea169f4fc53754a73aee542310bc2633c36a
Bart Piasek <bartpiasek@icloud.com>
2021-03-08 20:53:37 +0100

    auth view, form validation

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,6 +8,7 @@ const navigationItems = (props) => (
       Bigmac React
     </NavigationItem>
     <NavigationItem link="/orders">Orders</NavigationItem>
+    <NavigationItem link="/auth">Auth</NavigationItem>
   </ul>
 );
 
