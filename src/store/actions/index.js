bdeaeb1d0eb392b8b750d8bb052385a4d2f59ce2
Bart Piasek <bartpiasek@icloud.com>
2021-03-08 22:28:04 +0100

    actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,2 +1,31 @@
-export { addIngredient, removeIngredient } from "./burgerBuilder";
-export {} from "./order";
+export {
+  addIngredient,
+  removeIngredient,
+  initIngredients,
+  setIngredients,
+  fetchIngredientsFailed,
+} from "./burger-actions";
+
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders,
+  fetchOrdersSuccess,
+  fetchOrdersFailure,
+  fetchOrdersStart,
+  purchaseBurgerStart,
+  purchaseBurgerSuccess,
+  purchaseBurgerFailure,
+} from "./order-actions";
+
+export {
+  auth,
+  logout,
+  logoutSucceed,
+  setAuthRedirectPath,
+  authCheckState,
+  authStart,
+  authSuccess,
+  authFailure,
+  checkAuthTimeout,
+} from "./auth-actions";
